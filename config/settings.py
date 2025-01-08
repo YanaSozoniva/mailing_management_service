b63e81f7408042efa82db198bcd3ab2ff0dcd2bc
Yana Sozonova <ynakochneva@mail.ru>
2025-01-08 14:54:49 +0500

    создание моделей Newsletter, Message, MailingRecipient

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
     "mailing",
 ]
 
@@ -104,11 +103,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- mailing/models.py
+++ mailing/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
-# Create your models here.
+
+class MailingRecipient(models.Model):
+    """Модель Получатель рассылок"""
+
+    email = models.EmailField(verbose_name="Email", help_text="Укажите вашу электронную почту")
+    first_name = models.CharField(max_length=100, verbose_name="Имя")
+    last_name = models.CharField(max_length=100, verbose_name="Фамилия")
+    surname = models.CharField(max_length=100, verbose_name="Отчество", null=True, blank=True)
+    comment = models.TextField(verbose_name="Комментарий", null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} email: {self.email}"
+
+    class Meta:
+        verbose_name = "получатель"
+        verbose_name_plural = "получатели"
+        ordering = ["last_name", "first_name"]
+
+
+class Message(models.Model):
+    """Модель Сообщение"""
+
+    subject_letter = models.CharField(max_length=200, verbose_name="Тема письма")
+    body_letter = models.TextField(verbose_name="Тело письма")
+
+    def __str__(self):
+        return f"{self.subject_letter}"
+
+    class Meta:
+        verbose_name = "сообщение"
+        verbose_name_plural = "сообщения"
+        ordering = ["subject_letter"]
+
+
+class Newsletter(models.Model):
+    """Модель Рассылка"""
+
+    CREATED = "create"
+    LAUNCHED = "launch"
+    COMPLETED = "complete"
+
+    STATUS_CHOICES = [
+        (CREATED, "Создана"),
+        (LAUNCHED, "Запущена"),
+        (COMPLETED, "Завершена"),
+    ]
+    name = models.CharField(max_length=50, verbose_name='Название рассылки')
+    created_at = models.DateTimeField(verbose_name="Дата и время создания первой рассылки", auto_now_add=True)
+    updated_at = models.DateField(verbose_name="Дата и время последней рассылки", auto_now=True)
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default=CREATED, verbose_name="статус рассылки")
+    message = models.ForeignKey(
+        to=Message, on_delete=models.SET_NULL, related_name="newsletters", verbose_name="Сообщение"
+    )
+    recipients = models.ManyToManyField(
+        to=MailingRecipient, related_name="newsletters", verbose_name="Получатели рассылки"
+    )
+
+    def __str__(self):
+        return f"{self.name}: Дата первой рассылкиЖ {self.created_at}, дата последней рассылки {self.updated_at}"
+
+    class Meta:
+        verbose_name = "рассылка"
+        verbose_name_plural = "рассылки"
+        ordering = ["name", "created_at", "updated_at"]
