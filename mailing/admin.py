c349db2699803244c2caf2c480ccc2a2c8b92914
Yana Sozonova <ynakochneva@mail.ru>
2025-01-28 15:23:52 +0500

    настройка отображения данных моделей в админке по получателям

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from mailing.models import MailingRecipient
 
-# Register your models here.
+
+@admin.register(MailingRecipient)
+class RecipientAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'surname', 'email', 'comment')
+    list_filter = ('email',)
+    search_fields = ('email', 'last_name', 'first_name')
